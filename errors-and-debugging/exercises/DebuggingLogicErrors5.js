3bd3de200dcfb58cb949730ee5c3c4f924690986
jmalone487 <jmalone487@gmail.com>
2024-06-19 21:00:08 -0500

    Commit message

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -17,7 +17,7 @@ if (fuelLevel >= 20000) {
 
 console.log("launchReady = ", launchReady);
 
-if (crewStatus && computerStatus === 'green'){
+if (crewStatus && computerStatus === 'green') {
    console.log('Crew & computer cleared.');
    launchReady = true;
 } else {
@@ -26,3 +26,21 @@ if (crewStatus && computerStatus === 'green'){
 }
 
 console.log("launchReady = ", launchReady);
+
+console.log("launchReady = ", launchReady);
+
+if (crewStatus && computerStatus === 'green') {
+   console.log('Crew & computer cleared.');
+   crewReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   crewReady = false;
+}
+console.log("crewReady = ", crewReady);
+
+if (launchReady && crewReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
