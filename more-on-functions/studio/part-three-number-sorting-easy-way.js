539f429ace354844a12894abe2ed4c2bf21129a1
jmalone487 <jmalone487@gmail.com>
2024-07-01 18:36:45 -0500

    Commit message

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -1,3 +1,21 @@
+// Define the selectionSortRecursive function
+function selectionSortRecursive(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    let minIndex = 0;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[minIndex]) {
+            minIndex = i;
+        }
+    }
+
+    [arr[0], arr[minIndex]] = [arr[minIndex], arr[0]];
+
+    return [arr[0]].concat(selectionSortRecursive(arr.slice(1)));
+}
+
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
@@ -32,3 +50,5 @@ let originalNums1 = [5, 10, 2, 42];
 let sortedNums1 = selectionSortRecursive(originalNums1.slice());
 console.log("Original Array:", originalNums1);
 console.log("Sorted Array:", sortedNums1);
+
+
